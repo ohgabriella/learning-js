9b5a18975145265c9c985893703d13eb569d9e79
Gabriella Barreto <gabriella.gomes@avenuecode.com>
2021-08-09 14:08:38 -0300

    organizando as pastas

---
+++ javascript/intermediary/desestruct.js
@@ -0,0 +1,10 @@
+const pessoa = {
+    nome: 'gabi',
+    sobrenome: 'barreto',
+    idade: 20,
+    profissao: 'qa'
+}
+
+let { nome, sobrenome, idade, profissao } = pessoa;
+
+console.log(nome, sobrenome, idade, profissao);
