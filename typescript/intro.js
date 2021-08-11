72b3fe53d7d725ff0d38767340e4f68d2d4c3344
Gabriella Barreto <gabriella.gomes@avenuecode.com>
2021-08-11 10:32:51 -0300

    instalando e utilizando typescript

---
+++ typescript/intro.js
@@ -0,0 +1,7 @@
+"use strict";
+function somar(num1, num2) {
+    return num1 + num2;
+}
+var num1 = 10;
+var num2 = 20;
+console.log(somar(num1, num2));
---
+++ typescript/intro.ts
@@ -0,0 +1,7 @@
+function somar(num1: number, num2: number) {
+    return num1+num2;
+}
+
+const num1 = 10; const num2 = 20;
+
+console.log(somar(num1,num2));
