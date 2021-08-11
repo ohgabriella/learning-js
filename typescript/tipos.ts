dfccccf2b4247aa8d5dfd5cfc9358b526df9ad86
Gabriella Barreto <gabriella.gomes@avenuecode.com>
2021-08-11 11:10:33 -0300

    tipo never

---
+++ typescript/tipos.ts
@@ -0,0 +1,64 @@
+/*os tipos sao declarados minusculos*/
+
+//boolean
+const contaPaga: boolean = false;
+
+//number
+const idade: number = 23;
+const avaliacao: number = 4.5;
+
+//string
+const nome: string = 'Gabriella';
+
+//array
+const idades: number[] = [23, 28, 45];
+const idades2: Array<number> = [23, 40, 45];
+
+//tuple
+let jogadores: [string, string];
+jogadores = ['Vitor', 'Fulano'];
+
+//enum
+enum StatusAprovacao {
+    Aprovado,
+    Pendente,
+    Rejeitado
+}
+const statusDaAprovacao: StatusAprovacao = StatusAprovacao.Pendente;
+
+// any
+const retornoDaApi: any[] = ['Gabriella', 28];
+const retornoDaApi2: any = {
+    //...
+};
+
+//void
+function printTela(msg: string): void {
+    console.log('msg')
+}
+
+// null e undefined
+const u: undefined = undefined;
+const n: null = null;
+
+// object - quando tem um tipo nao primitivo e voce precisa armazenar ele
+function criar(objeto: object) {
+    // ...
+}
+
+criar({
+    propriedade: 1,
+})
+
+// never - uma funcao que nunca termina
+function loopInfinito(): never {
+    while(true) { }
+}    
+
+function erro(mensagem: string): never {
+    throw new Error(mensagem);
+}
+
+function falha() {
+    return erro('algo deu errado');
+}
