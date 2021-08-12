618bb7ce29111fdb8a95866e1f0eabd301c1250a
Gabriella Barreto <gabriella.gomes@avenuecode.com>
2021-08-11 22:30:25 -0300

    add interface

--- typescript/classes.ts
+++ typescript/classes.ts
@@ -1,8 +1,20 @@
 class User {
     private cpf: number;
-    constructor(public name: string, public lastName: string) {
 
+    constructor(public name: string, public lastName: string) {
+        this.cpf = 1234567800;
     }
 }
 
 const user = new User('Gabriella', 'Barreto');
+
+// Heranca
+class Person extends User {
+
+    constructor() {
+        super('Ariel', 'Soares')
+    }
+
+    
+    
+}
